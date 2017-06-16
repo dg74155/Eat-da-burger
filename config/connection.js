96d2cdbaf0d46e79aeb8e2558c755e252f19fcec
Derek Greene <dg74155@gmail.com>
2017-06-15 21:22:20 -0400

    jaws

--- config/connection.js
+++ config/connection.js
@@ -1,11 +1,18 @@
 var mysql = require("mysql");
 
-var connection = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "Ufcvette1!",
-  database: "burgers_db"
-});
+if (process.env.JAWSDB_URL) {
+    // DB is JawsDB on Heroku
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+    // DB is local on localhost
+    connection = mysql.createConnection({
+        port: 3306,
+        host: 'localhost',
+        user: 'root',
+        password: 'Ufcvette1!',
+        database: 'burgers_db'
+    })
+};
 
 connection.connect(function(err) {
   if (err) {
